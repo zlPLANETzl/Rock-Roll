b8d8d238285a5e8049fff55e3afd35ecb684c5c6
zlPLANETzl <86148853+zlPLANETzl@users.noreply.github.com>
2025-03-18 12:29:21 +0900

    feat: 모델링 폴더 및 fbx, 카메라 시점, 캐릭터 방향 전환

--- Assets/Scripts/PlayerMove.cs
+++ Assets/Scripts/PlayerMove.cs
@@ -5,6 +5,7 @@ using UnityEngine;
 public class PlayerMove : MonoBehaviour
 {
     public float playerMoveSpeed = 7f;
+    public Transform modelController;
 
     private CharacterController cc;
 
@@ -17,11 +18,21 @@ public class PlayerMove : MonoBehaviour
     // Update is called once per frame
     void Update()
     {
-        float h = Input.GetAxis("Horizontal");
-        float v = Input.GetAxis("Vertical");
+        float h = Input.GetAxisRaw("Horizontal"); // Raw 입력 사용
+        float v = Input.GetAxisRaw("Vertical");   // Raw 입력 사용
 
         Vector3 dir = new Vector3(h, 0, v).normalized;
-        
         cc.Move(dir * playerMoveSpeed * Time.deltaTime);
+
+        if (dir != Vector3.zero)
+        {
+            // 이동 방향에 따른 y축 각도 계산 (라디안->도)
+            float targetYAngle = Mathf.Atan2(dir.x, dir.z) * Mathf.Rad2Deg;
+            // 기존 localRotation의 값을 그대로 가져옴
+            Vector3 localEuler = modelController.localEulerAngles;
+            // y축만 targetYAngle로 수정
+            localEuler.y = targetYAngle;
+            modelController.localEulerAngles = localEuler;
+        }
     }
 }
